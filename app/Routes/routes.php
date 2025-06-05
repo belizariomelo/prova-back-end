93fde9eab50229898b0ea86e5af4a0e6ea65d797
Belizariomorais <124016228+belizariomelo@users.noreply.github.com>
2025-06-05 18:35:23 -0300

    Update routes.php

--- app/Routes/routes.php
+++ app/Routes/routes.php
@@ -6,7 +6,7 @@ use App\Controllers\TransacaoController;
 
 return function (App $app) {
     // Rota para criar transação
-  
+    $app->post('/transacao', [TransacaoController::class, 'criar']);
 
     // Rota para buscar uma transação por ID
     $app->get('/transacao/{id}', [TransacaoController::class, 'buscar']);
