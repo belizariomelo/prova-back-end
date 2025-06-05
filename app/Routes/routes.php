e0871af87b0a2413666f1a19fd38039bc4ace0bd
Belizariomorais <124016228+belizariomelo@users.noreply.github.com>
2025-06-05 18:35:47 -0300

    Update routes.php

--- app/Routes/routes.php
+++ app/Routes/routes.php
@@ -8,7 +8,9 @@ return function (App $app) {
     // Rota para criar transação
     $app->post('/transacao', [TransacaoController::class, 'criar']);
 
-   
+   // Rota para buscar uma transação por ID
+    $app->get('/transacao/{id}', [TransacaoController::class, 'buscar']);
+    
     // Rota para apagar todas as transações
     $app->delete('/transacao', [TransacaoController::class, 'apagarTudo']);
 
